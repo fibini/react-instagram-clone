2cdc0d0029ea4e658854671c2d94705fc46d0319
Fabien <fabienswift@gmail.com>
2023-06-15 18:30:31 -0400

    adjust status bar

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -13,7 +13,7 @@ export default function Status()
     return(
         <>
         <Swiper
-        slidesPerView={7}
+        slidesPerView={4}
         spaceBetween={5}
         pagination={{
           clickable: true,
@@ -21,49 +21,49 @@ export default function Status()
         // modules={[Pagination]}
         className=" h-20 px-3"
         >
-        <SwiperSlide><img src={cinema} alt="" className=" h-14 w-14 rounded-full" /></SwiperSlide>
+        <SwiperSlide><img src={cinema} alt="" className=" h-16 w-16 rounded-full" /></SwiperSlide>
         <SwiperSlide>
             <div className="flex flex-col text-center">
-                <div className=" flex justify-center items-center h-16 rounded-full border-2 border-red-500">
+                <div className=" flex justify-center items-center h-20 w-20 rounded-full border-2 border-red-500">
                     <img src={gaming} alt="" className=" h-5/6 w-5/6 rounded-full" />
                 </div>
             </div>
             
             </SwiperSlide>
         <SwiperSlide>
-            <div className=" flex justify-center items-center h-16 rounded-full border-2 border-red-500">
+            <div className=" flex justify-center items-center h-20 w-20 rounded-full border-2 border-red-500">
                 <img src={akuma} alt="" className=" h-5/6 w-5/6 rounded-full" />
             </div>
             <p></p>
             </SwiperSlide>
         <SwiperSlide>
-            <div className=" flex justify-center items-center h-16 rounded-full border-2 border-red-500">
+            <div className=" flex justify-center items-center h-20 w-20 rounded-full border-2 border-red-500">
                 <img src={mans} alt="" className=" h-5/6 w-5/6 rounded-full" />
             </div>
             <p></p>
             </SwiperSlide>
         <SwiperSlide>
-            <div className=" flex justify-center items-center h-16 rounded-full border-2 border-red-500">
+            <div className=" flex justify-center items-center h-20 w-20 rounded-full border-2 border-red-500">
                 <img src={tokyo} alt="" className=" h-5/6 w-5/6 rounded-full" />
             </div>
             </SwiperSlide>
         <SwiperSlide>
-            <div className=" flex justify-center items-center h-16 rounded-full border-2 border-red-500">
+            <div className=" flex justify-center items-center h-20 w-20 rounded-full border-2 border-red-500">
                 <img src={zimmer} alt="" className=" h-5/6 w-5/6 rounded-full" />
             </div>
             </SwiperSlide>
         <SwiperSlide>
-            <div className=" flex justify-center items-center h-16 rounded-full border-2 border-red-500">
+            <div className=" flex justify-center items-center h-20 w-20 rounded-full border-2 border-red-500">
                 <img src={samus} alt="" className=" h-5/6 w-5/6 rounded-full" />
             </div>
             </SwiperSlide>
         <SwiperSlide>
-            <div className=" flex justify-center items-center h-16 rounded-full border-2 border-red-500">
+            <div className=" flex justify-center items-center h-20 w-20 rounded-full border-2 border-red-500">
                 <img src="" alt="" className=" h-5/6 w-5/6" />
             </div>
             </SwiperSlide>
         <SwiperSlide>
-            <div className=" flex justify-center items-center h-16 rounded-full border-2 border-red-500">
+            <div className=" flex justify-center items-center h-20 w-20 rounded-full border-2 border-red-500">
                 <img src="" alt="" className=" h-5/6 w-5/6" />
             </div>
             </SwiperSlide>
