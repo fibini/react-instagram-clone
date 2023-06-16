189d8ae4851aa5d3dc5ea4fccc99eb0776eb12bb
Fabien <fabienswift@gmail.com>
2023-06-16 15:33:54 -0400

    adjust bottom margin

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Footer from "./components/Footer"
 export default function App() {
   return (
     <>
-      <div className=" mb-10">
+      <div className=" mb-15">
         <Nav />
         <Status />
         <Posts />
